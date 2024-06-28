54dabce84136b3f31e26974d8770c22e91638845
Anthony Stolp <anthony.stolp@gmail.com>
2024-06-28 12:07:47 -0400

    setup backend for call signature

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ app.use(bodyParser.json());
 app.use(cors()); // Enable CORS
 
 cloudinary.config({
-  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
-  api_secret: process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 app.get("/get-signature", (req, res) => {
